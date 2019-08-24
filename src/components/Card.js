20e11bb3527131b68120fa9651ee6b42045797af
Luis Hernández <ldhdv95@gmail.com>
2019-08-24 11:46:00 -0500

    Import fonts in App.css

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,10 +1,16 @@
 import React from "react";
 
 function Card(props) {
-  const { name, id } = props;
+  const { id, photo, name, city, price, rating } = props;
   return (
     <div id={id}>
-      <p>{name}</p>
+      <img src={photo} alt={name} />
+      <div>
+        <span>{city}</span>
+        <h2>{name}</h2>
+        <p>Desde COP ${price}</p>
+        <span>{rating}</span>
+      </div>
     </div>
   );
 }
