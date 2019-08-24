105db8c83a6600566e387c98e1dd72dfd4ee9a70
Luis Hernández <ldhdv95@gmail.com>
2019-08-24 09:58:12 -0500

    Fetch items from API

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import Main from "./components/Main";
+import "./App.css";
 
 function App() {
   return (
@@ -17,6 +18,7 @@ function App() {
           rel="noopener noreferrer"
         >
           Learn React
+          <Main />
         </a>
       </header>
     </div>
--- src/components/Main.js
+++ src/components/Main.js
@@ -1,7 +1,29 @@
 import React, { Component } from "react";
+import axios from "axios";
 
 class Main extends Component {
+  state = {
+    url: "https://limitless-beyond-13402.herokuapp.com/products",
+    items: [],
+  };
+
+  componentDidMount() {
+    this.fetchGet();
+  }
+
+  fetchGet = async () => {
+    const { url } = this.state;
+    try {
+      const response = await axios.get(url);
+      this.setState({ ...response.data });
+      console.log(response);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
   render() {
+    console.log(this.state);
     return (
       <section>
         <h1>Main Page</h1>
