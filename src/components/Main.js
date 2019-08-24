b505949591f44e95e07f4f76d7179168885ab376
Luis Hernández <ldhdv95@gmail.com>
2019-08-24 09:48:15 -0500

    Base components structure

---
+++ src/components/Main.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+
+class Main extends Component {
+  render() {
+    return (
+      <section>
+        <h1>Main Page</h1>
+      </section>
+    );
+  }
+}
+
+export default Main;
